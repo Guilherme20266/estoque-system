fafd5f6cc5d403a394b8cd59ba86b74915e0f3c1
Guilherme20266 <guiga31052000@gmail.com>
2026-06-03 17:23:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ class Historico(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     data = db.Column(db.String(30))
+    usuario = db.Column(db.String(100))
     acao = db.Column(db.String(50))
     produto = db.Column(db.String(200))
     quantidade = db.Column(db.Integer)
@@ -198,6 +199,7 @@ def cadastrar():
 
         historico = Historico(
             data=datetime.now().strftime("%d/%m/%Y %H:%M"),
+            usuario=session.get('usuario'),
             acao="CADASTRO",
             produto=produto.nome,
             quantidade=produto.quantidade,
@@ -318,6 +320,7 @@ def movimentacao():
 
             historico = Historico(
                 data=datetime.now().strftime("%d/%m/%Y %H:%M"),
+                usuario=session.get('usuario'),
                 acao="ENTRADA",
                 produto=produto.nome,
                 quantidade=quantidade,
@@ -333,6 +336,7 @@ def movimentacao():
 
             historico = Historico(
                 data=datetime.now().strftime("%d/%m/%Y %H:%M"),
+                usuario=session.get('usuario'),
                 acao="SAIDA",
                 produto=produto.nome,
                 quantidade=quantidade,
@@ -391,6 +395,7 @@ def transferencia():
 
         historico = Historico(
             data=datetime.now().strftime("%d/%m/%Y %H:%M"),
+            usuario=session.get('usuario'),
             acao="TRANSFERENCIA",
             produto=produto.nome,
             quantidade=produto.quantidade,
